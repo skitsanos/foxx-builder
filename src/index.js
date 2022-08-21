9760f9bcb15c534fcd3b6f663e7e43660d9228c3
skitsanos <info@skitsanos.com>
2022-08-21 13:34:45 +0300

    chore: cleanup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 const builder = require('./builder/index');
 builder.init();
 
-const sessions = require('./sessions/index');
-sessions.allowedResources = [
-    ...sessions.allowedResources,
-    '/echo',
-    /\/examples(\/.*)?/gi,
-    /\/users(\/.*)?/gi
-];
-
-sessions.init();
-
 /**
  * Run Google Analytics on each API endpoint request
  */
