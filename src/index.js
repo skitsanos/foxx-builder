49877d7827eb528a6cc86997b0ab2b6562cfa2eb
skitsanos <info@skitsanos.com>
2022-08-21 15:06:38 +0300

    feat: Example on how to use Netlify

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 const builder = require('./builder/index');
 builder.init();
 
-
-/*module.context.use((req, res, next) =>
+module.context.use((req, res, next) =>
 {
-    const {runTask} = module.context;
-    runTask(
-        'Apilitics via Amplitude',
-        'amplitude',
-        {
-            userId: Boolean(req.session && req.session.uid) ? req.session.uid : '(anonymous)',
-            authorized: Boolean(req.session && req.session.uid),
-            path: req.path,
-            headers: req.headers,
-            events: [
-                {
-                    type: 'prod.site.pageview'
-                }
-            ]
-        });
+    const apiKey = req.headers['x-api-key'];
 
+    if (!apiKey || apiKey !== 'some-api-key-string')
+    {
+        res.throw(400, 'Invalid API key');
+    }
     next();
-});*/
+});
+
