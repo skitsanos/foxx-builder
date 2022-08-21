d11d865e97a75315379fa385924df8a1075c436d
skitsanos <info@skitsanos.com>
2022-08-21 14:25:05 +0300

    chore: cleanup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 const builder = require('./builder/index');
 builder.init();
 
-/**
- * Run Google Analytics on each API endpoint request
- */
-/*
- module.context.use((req, res, next) =>
- {
- const {runTask} = module.context;
- runTask(
- 'Google Analytics PageView recording',
- 'ga',
- {
- clientId: req.headers['x-bb-client-request-uuid'],
- path: req.path,
- headers: req.headers
- });
-
- next();
- });*/
 
 /*module.context.use((req, res, next) =>
 {
