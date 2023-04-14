838fb3b0c6701552c897445289ad43270d9ec55b
skitsanos <info@skitsanos.com>
2023-04-14 15:53:15 +0300

    feat: GraphQL support with examples

--- src/routes/examples/graphql_demo/graphql.js
+++ src/routes/examples/graphql_demo/graphql.js
@@ -1,66 +1,85 @@
-const {buildSchema} = require('graphql');
+const {GraphQLString, GraphQLSchema, GraphQLObjectType} = require('graphql');
 const {query} = require('@arangodb');
 
-// Define the GraphQL schema
-const schema = buildSchema(`
-  type Article {
-    _key: String!
-    title: String
-    authorKey: ID!
-    author: Author
-  }
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: {
+        _key: {type: GraphQLString},
+        name: {type: GraphQLString}
+    },
+    resolve: (_, author) =>
+    {
+        const result = query`
+        FOR author IN Authors
+        FILTER author._key == ${author._key}
+        RETURN author
+        `;
+        return result.next();
+    }
+});
 
-  type Author {
-    _key: String!
-    name: String
-  }
-
-  type Query {
-    hello: String
-    getArticle(_key: String!): Article
-    allArticles: [Article]
-  }
-`);
-
-// Define the resolvers for the schema
-const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-
-        getArticle: ({_key}) =>
-        {
-            const [result] = query`
-             FOR article IN Articles
-                FILTER article._key == ${_key}
-             RETURN article
-            `.toArray();
-
-            return result;
-        },
-
-        allArticles: () =>
-        {
-            return query`
-            FOR article IN Articles 
-            RETURN article`.toArray();
+const ArticleType = new GraphQLObjectType({
+    name: 'Article',
+    fields: {
+        _key: {type: GraphQLString},
+        title: {type: GraphQLString},
+        authorKey: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve: (parent) =>
+            {
+                const authorKey = parent.authorKey;
+                const result = query`
+                FOR author IN Authors
+                FILTER author._key == ${authorKey}
+                RETURN author
+                `;
+                return result.next();
+            }
         }
     },
-
-    Article: {
-        author: (article) =>
-        {
-            const [result] = query`
-            FOR author IN Authors
-                FILTER author._key == ${article.authorKey}}
-            RETURN author
-            `;
-            return result;
-        }
+    resolve: (_, article) =>
+    {
+        const result = query`
+        FOR author IN Authors
+        FILTER author._key == ${article.authorKey}}
+        RETURN author
+        `;
+        return result.next();
     }
-};
+});
 
 module.exports = {
-    schema,
-    rootValue: resolvers.Query,
+    schema: new GraphQLSchema({
+        query: new GraphQLObjectType({
+            name: 'RootQueryType',
+            fields: {
+                hello: {
+                    type: GraphQLString,
+                    resolve()
+                    {
+                        return 'world';
+                    }
+                },
+
+                getArticle: {
+                    type: ArticleType,
+
+                    args: {
+                        _key: {type: GraphQLString}
+                    },
+
+                    resolve(_, {_key})
+                    {
+                        const result = query`FOR article IN Articles
+                            FILTER article._key == ${_key}
+                            RETURN article`;
+                        return result.next();
+                    }
+
+                }
+            }
+        })
+    }),
     graphiql: true
 };
